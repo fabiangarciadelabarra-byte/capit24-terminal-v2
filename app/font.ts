28db3dc3b3a6e489a81c88001d2989339110c399
fabiangarciadelabarra-byte <fabiangarciadelabarra@gmail.com>
2026-05-18 10:08:07 -0500

    Create font.ts

---
+++ app/font.ts
@@ -0,0 +1,11 @@
+import { GeistSans, GeistMono } from "geist/font";
+
+export const geistSans = GeistSans({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const geistMono = GeistMono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
