2b42f3346d72d026aea5337d379c02d2fb2f9553
Fabian Garcia <fabiangarciadelabarra@gmail.com>
2026-05-18 11:17:16 -0500

    Fix: correct Geist font usage

--- app/font.ts
+++ app/font.ts
@@ -1,11 +1,4 @@
 import { GeistSans, GeistMono } from "geist/font";
 
-export const geistSans = GeistSans({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-export const geistMono = GeistMono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export const geistSans = GeistSans;
+export const geistMono = GeistMono;
