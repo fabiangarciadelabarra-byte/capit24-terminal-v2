8c2f933dce8df1e614e7a046337c5c7dafab201a
Fabian Garcia <fabiangarciadelabarra@gmail.com>
2026-07-01 10:46:55 -0500

    fix tailwind 4 postcss plugin

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,14 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import tailwindPostcss from "@tailwindcss/postcss";
 
 const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    tailwindPostcss: {},
   },
 };
 
 export default config;
+
